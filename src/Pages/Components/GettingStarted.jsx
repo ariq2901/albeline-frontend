e8b9cb616cc6f555a306d2cbca2bae9806b16c9e
Ariq2901 <ariqkage@gmail.com>
2021-03-03 13:28:05 +0700

    final destination

--- src/Pages/Components/GettingStarted.jsx
+++ src/Pages/Components/GettingStarted.jsx
@@ -39,14 +39,18 @@ export const GettingStarted = () => {
       {seller_type === 'new' && (
         <div className="getting-started">
           {/* <button onClick={removePopup}>Let's do this</button> */}
-          <div className="sementara gs-vector">
+          <div className="sementara gs-vector mt-5">
             <div className="gs-img-wrap">
               <img src={NewSeller} alt="vector"/>
             </div>
           </div>
-          <div className="gs-title">
+          <div className="gs-title text-center mt-3" style={{ fontSize: '20px', fontFamily: 'Nunito-Bold', color: 'var(--secondary-color)' }}>
             <span>Welcome New Member</span>
           </div>
+          <div className="gs-content text-center mt-1 p-3" style={{ fontFamily: 'Nunito-Semibold', color: '#8a8ebb' }}>
+            <p>here you can monitor and manage all your store products. You can Manage your order, product list, and settings the store here</p>
+          </div>
+          <button onClick={removePopup} className="mt-2">Let's Do This</button>
         </div>
       )}
     </Fragment>
--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 import Cookie from 'universal-cookie';
 var cookies = new Cookie();
 const config = {
-  // api_host:'https://albeline-backend.herokuapp.com',
-  // api_rajaongkir: 'https://api.rajaongkir.com/starter'
-  api_host:'http://127.0.0.1:8000',
+  api_host:'https://albeline-backend.herokuapp.com'
+  // api_host:'http://127.0.0.1:8000',
 }
 export {config};
